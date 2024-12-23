3fa73650c861ef44ea444dd747eaad3fe8604031
Dhruv Dhaduk <dhadukd44@gmail.com>
2024-12-23 23:39:38 +0530

    Moved the tokyonight.nvim config and adding to its seperate file

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,21 +24,6 @@ vim.g.maplocalleader = "\\"
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    { 
-      "folke/tokyonight.nvim", 
-      config = function() 
-        require("tokyonight").setup({
-          style = "night",
-          on_highlights = function(highlights, colors)
-            highlights.LineNr = { fg = "#8e96f0", bold = true } 
-            highlights.LineNrAbove = { fg = "#7077be" } 
-            highlights.LineNrBelow = { fg = "#7077be" } 
-          end
-        })
-
-        vim.cmd.colorscheme "tokyonight" 
-      end 
-    },
     { import = "config.plugins" },
   },
 })
---
+++ lua/config/plugins/tokyonight.lua
@@ -0,0 +1,17 @@
+return {
+  { 
+    "folke/tokyonight.nvim", 
+    config = function() 
+    require("tokyonight").setup({
+      style = "night",
+      on_highlights = function(highlights, colors)
+        highlights.LineNr = { fg = "#8e96f0", bold = true } 
+        highlights.LineNrAbove = { fg = "#7077be" } 
+        highlights.LineNrBelow = { fg = "#7077be" } 
+      end
+    })
+
+    vim.cmd.colorscheme "tokyonight" 
+    end 
+  }
+}
