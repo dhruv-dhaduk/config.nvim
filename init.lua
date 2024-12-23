53604158759467137151e2251acbb3a99d773b67
Dhruv Dhaduk <dhadukd44@gmail.com>
2024-12-23 17:33:26 +0530

    Created basic initial configs, key remaps and added highlight on yank

---
+++ init.lua
@@ -0,0 +1,21 @@
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+vim.cmd("syntax on")
+vim.cmd("colorscheme industry")
+
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function() 
+    vim.highlight.on_yank()
+  end,
+})
