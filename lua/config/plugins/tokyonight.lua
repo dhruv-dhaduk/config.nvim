a5885aab36d663bf0a0e39a9271bfcd13fca88f1
Dhruv Dhaduk <dhadukd44@gmail.com>
2024-12-25 14:32:26 +0530

    Changed the color of line numbers from tokyonight.nvim plugin

--- lua/config/plugins/tokyonight.lua
+++ lua/config/plugins/tokyonight.lua
@@ -1,16 +1,16 @@
 return {
-  { 
-    "folke/tokyonight.nvim", 
-    config = function() 
+  {
+    "folke/tokyonight.nvim",
+    config = function()
       require("tokyonight").setup({
         style = "night",
         on_highlights = function(highlights, colors)
-          highlights.LineNr = { fg = "#8e96f0", bold = true } 
-          highlights.LineNrAbove = { fg = "#7077be" } 
-          highlights.LineNrBelow = { fg = "#7077be" } 
+          highlights.LineNr = { fg = "#ffffff", bold = true }
+          highlights.LineNrAbove = { fg = "#8e96f0" }
+          highlights.LineNrBelow = { fg = "#8e96f0" }
         end
       })
-      vim.cmd.colorscheme "tokyonight" 
-    end 
+      vim.cmd.colorscheme "tokyonight"
+    end
   }
 }
