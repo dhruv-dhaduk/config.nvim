674b7af296b7889e8f9a147fedea3ac8323fa6cc
Dhruv Dhaduk <dhadukd44@gmail.com>
2024-12-30 16:17:58 +0530

    Changed a keymap in telescope

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -21,7 +21,7 @@ return {
 
       vim.keymap.set("n", "<space>fd", require('telescope.builtin').find_files)
       vim.keymap.set("n", "<space>fh", require('telescope.builtin').help_tags)
-      vim.keymap.set("n", "<space>en", function()
+      vim.keymap.set("n", "<space>ne", function()
         require('telescope.builtin').find_files {
           cwd = vim.fn.stdpath("config")
         }
