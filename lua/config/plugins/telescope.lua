408f027d140421545909d077ac23e247cc4b99e4
Dhruv Dhaduk <dhadukd44@gmail.com>
2025-01-01 21:44:44 +0530

    Created multigrep searching option in telescope

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -31,6 +31,14 @@ return {
           cwd = vim.fn.stdpath("config")
         }
       end)
+
+      vim.keymap.set("n", "<space>ep", function()
+        require('telescope.builtin').find_files {
+          cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
+        }
+      end)
+
+      require "config.telescope.multigrep".setup()
     end
   }
 }
