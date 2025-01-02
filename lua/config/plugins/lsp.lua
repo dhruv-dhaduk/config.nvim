7a026675e22e91ba4b57e10241feb017986ecc6b
Dhruv Dhaduk <dhadukd44@gmail.com>
2025-01-02 16:40:30 +0530

    Added completion using blink.cmp plugin

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,20 +1,23 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    dependencies = { {
-      "folke/lazydev.nvim",
-      ft = "lua",
-      opts = {
-        library = {
-          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+    dependencies = {
+      'saghen/blink.cmp',
+      {
+        "folke/lazydev.nvim",
+        ft = "lua",
+        opts = {
+          library = {
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
         },
       },
     },
-    },
     config = function()
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
       local lsp = require("lspconfig")
-      lsp.lua_ls.setup {}
-      lsp.ccls.setup {}
+      lsp.lua_ls.setup { capabilities = capabilities }
+      lsp.ccls.setup { capabilities = capabilities }
 
       vim.api.nvim_create_autocmd('LspAttach', {
         callback = function(args)
