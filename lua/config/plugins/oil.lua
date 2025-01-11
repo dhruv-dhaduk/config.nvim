846768f446df11c3fec197321e076025eccfc04d
Dhruv Dhaduk <dhadukd44@gmail.com>
2025-01-11 16:01:56 +0530

    Enabled the hidden files in oil.nvim

--- lua/config/plugins/oil.lua
+++ lua/config/plugins/oil.lua
@@ -7,5 +7,13 @@ return {
     -- Optional dependencies
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+
+    config = function()
+      require("oil").setup({
+        view_options = {
+          show_hidden = true
+        }
+      })
+    end
   }
 }
