aeff587bdbfef628f120c0d89ae1a81466603431
Dhruv Dhaduk <dhadukd44@gmail.com>
2025-03-09 18:56:52 +0530

    Added keymap to unhighlight the text and Added condition before executing prettier

--- init.lua
+++ init.lua
@@ -21,12 +21,18 @@ vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
 
 vim.keymap.set("n", "<space><space>y", "<cmd>%y<CR>")
+vim.keymap.set("n", "<Esc>", "<cmd>:nohlsearch<CR>")
 
 vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = { "*.js", "*.jsx", "*.ts", "*.tsx", "*.json", "*.css", "*.scss", "*.html" },
+
   callback = function()
-    vim.fn.system("npx prettier --write " .. vim.fn.fnamemodify(vim.fn.expand("%"), ":."))
-    vim.cmd("edit")
+    local marker_path = vim.fn.getcwd() .. "/" .. ".prettierrc"
+    local has_marker = vim.fn.filereadable(marker_path) == 1
+    if has_marker then
+      vim.fn.system("npx prettier --write " .. vim.fn.fnamemodify(vim.fn.expand("%"), ":."))
+      vim.cmd("edit")
+    end
   end,
 })
 
